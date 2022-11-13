7341220db3bccc127a0e1a9367a086ea1590de81
nicolaspc-dev <nicolaspc.wd@gmail.com>
2022-11-12 20:53:02 -0500

    Comments added

--- app/src/main/java/com/kotlin/shophoschallenge01/Challenge.kt
+++ app/src/main/java/com/kotlin/shophoschallenge01/Challenge.kt
@@ -1,46 +1,54 @@
 package com.kotlin.shophoschallenge01
 
 fun main(){
-  val textType: String
-  var numSequence = ""
+  val typeA = "TypeA"
+  val typeB = "TypeB"
+  val numSequence: String
+  val sortingCommand: String
   var dataValue = 0
   var validInput = false
+  val dataText = "Which kind of number sequence do you want? \n1. TypeA = \"54********\"\n2. TypeB = \"08********\""
+  val sortingText = "\n:::Great ! Now let's sort this numeric sequence:::\nType one of the following commands: \"Asc\" or \"Desc\"."  +
+             "\n* Asc = Ascending order * Desc = Descending order"
 
+  // Input data validation.
   while(!validInput){
-    println("Which kind of number sequence do you want? " +
-            "\n1. TypeA = \"54********\" " +
-            "\n2. TypeB = \"08********\"" )
+    println(dataText)
     dataValue = readln().toInt()
     if(dataValue == 1 || dataValue == 2) validInput = true else continue
   }
 
+  // Conditional initialization of "textType" variable & functions execution.
   when(dataValue){
     1 -> {
-      textType = "TypeA"
-      numSequence = numSequenceGenerator(textType)
+      numSequence = numSequenceGenerator(typeA)
+      println(sortingText)
+      sortingCommand = readln()
+      numSorting(numSequence, sortingCommand)
     }
     2 -> {
-      textType = "TypeB"
-      numSequence = numSequenceGenerator(textType)
+      numSequence = numSequenceGenerator(typeB)
+      println(sortingText)
+      sortingCommand = readln()
+      numSorting(numSequence, sortingCommand)
     }
   }
-
-  println("\nGreat ! Now let's sort this numeric sequence. " +
-          "\nType one of the following commands: \"Asc\" or \"Desc\"."  +
-          "\n* Asc = Ascending order * Desc = Descending order")
-  val sortingCommand : String = readln()
-
-  numSorting(numSequence, sortingCommand)
-
 }
+
 
 fun numSequenceGenerator(textType: String): String{
   var sequenceValue = ""
+  // ** Numeric Sequence Creation **
+  // 1. Generate a Random #, then multiply by 10 and set it into a String type.
   val randomSequence = ((Math.random()*10).toString())
+    // 2. The numeric String is separated by it's dot, this return an array with 2 values.
     .split(".")
+    // 3. Put together both values into a single value as a String.
     .joinToString("")
+    // 4. Take the first eight values of the string.
     .take(8)
 
+  // Conditional sequence generation
   when(textType){
     "TypeA" -> {
       sequenceValue = "54$randomSequence"
@@ -55,14 +63,20 @@ fun numSequenceGenerator(textType: String): String{
 }
 
 fun numSorting(numSequence : String, sortingCommand: String) {
+  // Convert the first parameter into a character array.
   val numArray : CharArray = numSequence.toCharArray()
+  // Auxiliary variable: this will holds temporary values to sort the array.
   var tmp: Char
 
   when(sortingCommand){
      "Asc" -> {
+       // Main loop: Iterate for each array element.
        for (x in numArray.indices) {
+         // Secondary loop: Iterate over the iterate element.
          for (y in numArray.indices) {
+           // Conditional sorting => Greater than.
            if (numArray[x] > numArray[y]) {
+             // Sorting values
              tmp = numArray[x]
              numArray[x] = numArray[y]
              numArray[y] = tmp
@@ -72,9 +86,13 @@ fun numSorting(numSequence : String, sortingCommand: String) {
        println("Ascending Order:")
      }
      "Desc" -> {
+       // Main loop: Iterate for each array element.
        for(x in numArray.indices){
+         // Secondary loop: Iterate over the iterate element.
          for(y in numArray.indices){
+           // Conditional sorting => Smaller than.
            if(numArray[x] < numArray[y]){
+             // Sorting values
              tmp = numArray[y]
              numArray[y] = numArray[x]
              numArray[x] = tmp
